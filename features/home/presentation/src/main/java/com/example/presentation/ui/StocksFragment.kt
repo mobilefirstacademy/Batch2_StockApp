c91d68fb68133a26ac62359cb5e55fa6b6b41498
Paxagi <paxagi@gmail.com>
2024-04-21 01:55:24 +0300

    fix: add all elements of stock fragment

--- features/home/presentation/src/main/java/com/example/presentation/ui/StocksFragment.kt
+++ features/home/presentation/src/main/java/com/example/presentation/ui/StocksFragment.kt
@@ -8,17 +8,18 @@ import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -27,15 +28,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.home.R
-import kotlinx.coroutines.channels.ticker
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -101,13 +104,14 @@ class Stock(
     val ticker: String,
     val name: String,
     val unitOfAccount: UnitOfAccount,
-    val price: Double,
-    val diff: Double,
+    val purchasePrice: Double,
+    val currentPrice: Double,
     val isFavourite: Boolean,
+    val imageResource: Int,
 )
 val basicStocks = listOf(
-    Stock("YNDX", "Yandex, LCC", UnitOfAccount.RUB, 4764.6, 1.15, false),
-    Stock("AAPL", "Apple Inc.", UnitOfAccount.USD, 131.93, 1.15, true),
+    Stock("YNDX", "Yandex, LCC", UnitOfAccount.RUB,4709.6, 4764.6, false, R.drawable.yandex),
+    Stock("AAPL", "Apple Inc.", UnitOfAccount.USD, 131.81, 131.93, true, R.drawable.apple),
 )
 
 @Preview
@@ -125,7 +129,7 @@ fun StocksPage() {
     ) {
         Column {
             InputSearch("Find company or ticker") // поисковая строка
-            Tabs(tabs = listOf("Stocks", "Favorite")) // вкладки
+            Tabs(tabs = listOf("Stocks", "Favorite"), 0) // вкладки
             StocksList(stocks = basicStocks)// TODO: список тикеров
             // TODO:
         }
@@ -145,7 +149,7 @@ fun InputSearch(placeholder: String, value: String = "") {
             modifier = Modifier
                 .padding(15.dp, 15.dp, 0.dp, 15.dp)
                 .size(25.dp),
-            painter = painterResource(id = R.drawable.refresh_icon), // TODO: change picture
+            painter = painterResource(id = R.drawable.search),
             contentDescription = "searching icon",
         )
         TextField(
@@ -164,19 +168,19 @@ fun InputSearch(placeholder: String, value: String = "") {
 }
 
 @Composable
-fun Tabs(tabs: List<String>) {
+fun Tabs(tabs: List<String>, currentIndex: Int) {
     Surface{
         Row {
-            tabs.forEach {
-                Tab(it)
-            }
+            Tab(tabs[0], true)
+            Tab(tabs[1], false)
         }
     }
 }
 
 @Composable
-fun Tab(name: String) {
-    Text(text = name)
+fun Tab(name: String, isCurrent: Boolean) {
+    val weight = if (isCurrent) FontWeight.Bold else null
+    Text(text = name, fontWeight = weight)
 }
 
 @Composable
@@ -184,42 +188,51 @@ fun StocksList(stocks: List<Stock>) {
     LazyColumn(
         Modifier.fillMaxWidth()
     ) {
-        items(stocks) {stock ->
-            StockItem(stock)
+        itemsIndexed(stocks) {index, stock ->
+            val color = if (index % 2 == 0) Color(0xFFF0F4F7) else Color.White
+            StockItem(stock, color)
         }
     }
 }
 
 @Composable
-fun StockItem(stock: Stock) {
+fun StockItem(stock: Stock, backgroundColor: Color) {
     stock.run {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
                 .background(
-                    color = Color(0xFFF0F4F7),
-                    shape = RoundedCornerShape(10.dp)
+                    color = backgroundColor,
+                    shape = RoundedCornerShape(15.dp)
                 )
                 .fillMaxWidth()
 
         ) {
             Row {
                 Image(
-                    painter = painterResource(id = R.drawable.refresh_icon), // TODO: change picture
+
+                    painter = painterResource(id = imageResource),
                     contentDescription = "searching icon",
                     modifier = Modifier
-                        .padding(5.dp)
+                        .padding(10.dp)
                         .size(45.dp)
+                        .clip(RoundedCornerShape(12.dp))
                 )
 
-                Column {
+                Column(
+                    modifier = Modifier.padding(10.dp)
+                ) {
                     Ticker(ticker, isFavourite)
                     CompanyName(name)
                 }
             }
-            Column {
-                Price(price, unitOfAccount)
-                Diff(diff)
+            Column (
+                modifier = Modifier.padding(10.dp)
+            ) {
+                val diff = String.format("%.2f", currentPrice / purchasePrice)
+                val absoluteDiff = currentPrice - purchasePrice
+                Price(currentPrice, unitOfAccount)
+                Diff(diff, absoluteDiff, unitOfAccount)
             }
         }
     }
@@ -227,11 +240,16 @@ fun StockItem(stock: Stock) {
 
 @Composable
 fun Ticker(ticker: String, isFavourite: Boolean) {
-    Row{
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ){
         Text(text = ticker)
         Text(
             text = "★",
             color = if (isFavourite) Color(0xFFFFCA1C) else Color(0xFFBABABA),
+            modifier = Modifier
+                .fillMaxHeight()
+                .align(Alignment.Top)
         )
     }
 }
@@ -248,10 +266,34 @@ fun Price(price: Double, unitOfAccount: UnitOfAccount) {
         UnitOfAccount.USD -> "$"
         UnitOfAccount.EUR -> "€"
     }
-    Text(text = "$price $sign")
+    Box(
+        contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier.fillMaxWidth()
+    ){ Text(text = priceFormat(unitOfAccount, price)) }
 }
 
+fun priceFormat(unit: UnitOfAccount, count: Double): String {
+    val preUnit = when(unit) {
+        UnitOfAccount.USD -> "$"
+        else -> ""
+    }
+    val postUnit = when(unit) {
+        UnitOfAccount.RUB -> " ₽"
+        UnitOfAccount.EUR -> "€"
+        UnitOfAccount.USD -> ""
+        else -> ""
+    }
+    return preUnit + String.format("%.2f", count) + postUnit
+}
 @Composable
-fun Diff(diff: Double) {
-    Text(text = "($diff)%", )
+fun Diff(diff: String, absoluteDiff: Double, unit: UnitOfAccount) {
+    val sign = if (absoluteDiff > 0) "+" else "-"
+    val fontColor = if (absoluteDiff > 0) Color(0xFF24B25D) else Color.Black
+    val absDiffFormatted: String = "$sign${priceFormat(unit, absoluteDiff)}"
+    Box(
+        contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Text(text = "$absDiffFormatted ($diff%)", color = fontColor)
+    }
 }
