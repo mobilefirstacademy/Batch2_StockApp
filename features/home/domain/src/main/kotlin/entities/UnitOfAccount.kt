360d88c184865d773b834c91b2a3e6b03e785eb1
Paxagi <paxagi@gmail.com>
2024-06-03 19:24:56 +0300

    draft: add Stock model, UoA model

---
+++ features/home/domain/src/main/kotlin/entities/UnitOfAccount.kt
@@ -0,0 +1,5 @@
+package entities
+
+enum class UnitOfAccount {
+    RUB, USD, EUR
+}
--- features/home/domain/src/main/kotlin/searching/Searching.kt
+++ features/home/domain/src/main/kotlin/searching/Searching.kt
@@ -1,9 +1,13 @@
 package searching
 
+import entities.Stock
+
 class Searching {
     companion object {
-        fun filterStocksByName(seq: String): Unit { // TODO: change to List<Stock>
+        fun filterStocksByName(seq: String):List<Stock> {
             // TODO: add body
+
+            return listOf(Stock())
         }
     }
 }
