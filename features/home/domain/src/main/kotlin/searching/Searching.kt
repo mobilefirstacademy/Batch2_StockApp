a3e380d8d5af6fa714e26334272fea40143bc374
paxagi <paxagi@gmail.com>
2024-06-03 08:32:49 +0300

    new: create searching class files, delete files from .idea directory

---
+++ features/home/domain/src/main/kotlin/searching/Searching.kt
@@ -0,0 +1,9 @@
+package searching
+
+class Searching {
+    companion object {
+        fun filterStocksByName(seq: String): Unit { // TODO: change to List<Stock>
+            // TODO: add body
+        }
+    }
+}
